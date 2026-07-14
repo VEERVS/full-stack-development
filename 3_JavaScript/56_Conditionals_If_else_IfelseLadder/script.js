f193d1f1dbac05cb286e5c15ce218306c80df95f
Veer Vardhan Singh <veer232007@gmail.com>
2026-07-14 23:48:40 +0530

    Implement decision making with JavaScript conditionals

---
+++ 3_JavaScript/56_Conditionals_If_else_IfelseLadder/script.js
@@ -0,0 +1,24 @@
+console.log("Hello I am condional tutorial")
+
+let age = 17;
+let grace = 2;
+
+age += grace
+
+console.log(age)
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)
+console.log(age%grace)
+
+// if((age+2)>18){
+//     console.log("You can drive");
+// }else{
+//     console.log("You cannot Drive");
+// }
+
+//Both above and below translate same
+
+age>18 ? console.log("You can drive"):console.log("You cannot Drive") 
