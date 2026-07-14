182a11362aa5c4da2beebde8346e861b2ff75fe9
Veer Vardhan Singh <veer232007@gmail.com>
2026-07-14 23:47:42 +0530

    Explore JavaScript fundamentals

---
+++ 3_JavaScript/54_Introduction_To_JavaScript/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+console.log("2 Code is running...")
+console.log("3 Code is running...")
+console.log("4 Code is running...")
---
+++ 3_JavaScript/54_Introduction_To_JavaScript/script.js
@@ -0,0 +1,20 @@
+alert("Hello Word");
+
+console.log("1 Code is running...")
+console.log("2 Code is running...")
+console.log("3 Code is running...")
+console.log("4 Code is running...")
+
+var a = prompt("Enter your number")
+
+var isTrue = confirm("Are you sure you want to blast your computer?")
+
+if(isTrue){
+    console.log("BOOOOOOOOOOOOOM!")
+}else{
+    console.log("PC is SAFE")
+}
+
+console.log("Your number is " + a)
+
+document.title = "PC BOOM!"
