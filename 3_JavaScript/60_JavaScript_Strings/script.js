d8de24c12d6fffb2539cf05879260d90d596017a
Veer Vardhan Singh <veer232007@gmail.com>
2026-07-15 14:09:21 +0530

    Practice string manipulation in JavaScript

---
+++ 3_JavaScript/60_JavaScript_Strings/script.js
@@ -0,0 +1,37 @@
+console.log("Time for strings")
+
+let a = "Shaurya"
+let b = "Veer"
+
+console.log(a[0], b[0])
+console.log(a[1], b[1])
+console.log(a[2], b[2])
+console.log(a[3], b[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+
+console.log(a.length)
+
+let real_name = "Veer"
+let bro = "Shaurya"
+
+console.log("My name is "+ real_name +" and my brothers name is " + bro)
+console.log(`My name is ${real_name} and my brothers name is ${bro}`)
+
+let c = "Santosh"
+
+console.log(c.toUpperCase())
+console.log(c.toLowerCase())
+
+console.log(c.length)
+
+console.log(c.slice(1,5))
+console.log(c.slice(1))
+
+console.log(c.replace("Sa", "23"))
+
+console.log(c.concat(a))
+console.log(c.concat(a,b,23,"game"))
+
+console.log(c) //Immutable
