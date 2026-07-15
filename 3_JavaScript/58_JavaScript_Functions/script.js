7f328a19e785d7d72bf81ba5f9bb663fb02dee45
Veer Vardhan Singh <veer232007@gmail.com>
2026-07-15 14:09:07 +0530

    Write reusable code using JavaScript functions

---
+++ 3_JavaScript/58_JavaScript_Functions/script.js
@@ -0,0 +1,28 @@
+function nice(name){
+    console.log("Hey "+ name +" is very good guy")
+    console.log("Hey "+ name +" is very good student")
+    console.log("Hey "+ name +" is very good gamer")
+    console.log("Hey "+ name +" is very good coder")
+}
+
+nice("Veer")
+nice("Shaurya")
+
+function sum(a,b){
+    // console.log(a+b);
+    return a+b;
+}
+
+// sum(5,7)
+
+result = sum(5,7)
+
+console.log("The sum of a and b is ", result)
+
+const funct1 = (x)=>{
+    console.log("I am an arrow function", x)
+}
+
+funct1(54)
+funct1(523)
+funct1(5453)
