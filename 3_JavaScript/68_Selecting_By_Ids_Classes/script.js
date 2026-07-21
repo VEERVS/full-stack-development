9e6d4545557bdb4675229bb508a723d3f269e62b
Veer Vardhan Singh <veer232007@gmail.com>
2026-07-21 17:51:59 +0530

    Learn DOM element selection using IDs and classes

---
+++ 3_JavaScript/68_Selecting_By_Ids_Classes/script.js
@@ -0,0 +1,19 @@
+// console.log("Veer")
+
+// let VeerVS = document.getElementsByClassName("box")
+// console.log(VeerVS)
+
+// VeerVS[2].style.backgroundColor = "red";
+
+// document.getElementById("red").style.backgroundColor="red"
+
+// document.querySelector(".box").style.backgroundColor="green"
+
+// console.log(document.querySelectorAll(".box"))
+
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor="green";
+// })
+
+e = document.getElementsByTagName("div")
+
