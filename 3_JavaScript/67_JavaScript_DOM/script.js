1e1622d5fea4f8746f57d0e72c174e8ca165395d
Veer Vardhan Singh <veer232007@gmail.com>
2026-07-21 17:51:54 +0530

    Explore JavaScript DOM manipulation techniques

---
+++ 3_JavaScript/67_JavaScript_DOM/script.js
@@ -0,0 +1,20 @@
+console.log("Hello World!")
+
+
+
+// document.body.lastElementChild
+// document.body.firstElementChild
+// document.body.firstElementChild.childNodes
+// document.body.firstElementChild.children
+// document.body.firstElementChild.children.parentElement
+
+
+// document.body.firstElementChild.children[3]
+// <div class=​"box">​BOX4​</div>​
+
+// document.body.firstElementChild.children[3].nextElementSibling
+// <div class=​"box">​BOX5​</div>​
+
+// document.body.firstElementChild.children[3].previousElementSibling
+// <div class=​"box">​BOX3​</div>​
+
